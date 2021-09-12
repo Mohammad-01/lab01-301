1fdef6afc4f24cf4afdaf2029dba2c1bdb53daf8
Mohammad-01 <moalhasan432@gmail.com>
2021-09-13 00:03:14 +0300

    lab01

--- my-app/components/HornedBeast.js
+++ my-app/components/HornedBeast.js
@@ -5,7 +5,8 @@ class HornedBeasts extends React.Component {
         return (
             <div>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt={this.props.keyword} title={this.props.horns} />
+                <img src={this.props.image_url} alt={this.props.keyword} 
+                 title={this.props.horns} />
                 <p>{this.props.description}</p>
             </div>
         )
