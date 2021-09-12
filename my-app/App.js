5c2268e570aff75b7752698c6bb581997d95e2cd
Mohammad-01 <moalhasan432@gmail.com>
2021-09-12 23:43:41 +0300

    add

---
+++ my-app/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Compnents/Header";
+import Main from "./Compnents/Main";
+import Footer from "./Compnents/Footer";
+import './App.css';
+
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                {/* <h1>**********</h1>*/}
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
+        )
+    }
+}
+
+export default App;
